4a876f39f81be1fcef3b30430aa2bd745c6a80b0
Muhammad Zafar <mzafar611@gmail.com>
2023-10-09 17:18:17 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import Chatbot from "./chatbot/bot";
+import Chatbot from "./chatbot/chatbot";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
